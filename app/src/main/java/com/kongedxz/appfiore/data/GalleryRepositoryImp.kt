76a0586b9d06c2ea1ece5b50ec7e9322e7a893be
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2025-08-06 11:50:24 -0300

    Changes in GalleryRepository signature - getPhoto doesn't require a name anymore. And might return null. - Implementation adapted to work with new LocalSource's signature.

--- app/src/main/java/com/kongedxz/appfiore/data/GalleryRepositoryImp.kt
+++ app/src/main/java/com/kongedxz/appfiore/data/GalleryRepositoryImp.kt
@@ -2,31 +2,58 @@ package com.kongedxz.appfiore.data
 
 import com.kongedxz.appfiore.data.local.photos.PhotoData
 import com.kongedxz.appfiore.data.local.photos.PhotosLocalSource
-import com.kongedxz.appfiore.domain.entity.DescribedPhoto
+import com.kongedxz.appfiore.domain.entity.Photo
 import com.kongedxz.appfiore.domain.repository.GalleryRepository
 
 class GalleryRepositoryImp(private val localSource: PhotosLocalSource): GalleryRepository {
 
-    val cache = mutableMapOf<String, List<PhotoData>>()
+    val cache = mutableMapOf<String, MutableList<PhotoData>>()
 
-    override suspend fun getPhotos(category: String): List<PhotoData> {
-        return cache[category]?: try {
-            cache[category] = localSource.getAllPhotos(category)
+    override suspend fun getPhotos(category: String): List<PhotoData> =
+        if (cache[category] == null) {
+            val allPhotosList = try {
+                localSource.getAllPhotos()
+            } catch(_: Exception) {
+                emptyList()
+            }
+
+            makeCaches(allPhotosList)
+
+            cache[category]?: emptyList()
+        } else {
             cache[category]!!
-        } catch (_: Exception) {
-            emptyList()
         }
+
+    override suspend fun getPhoto(name: String): Photo? {
+        var photoDataList = emptyList<PhotoData>()
+
+        cache.entries.forEach {
+            photoDataList = photoDataList + it.value
+        }
+
+        val photoData = photoDataList.first {
+            it.name == name
+        }
+
+        return photoData.toDomainPhoto(
+            localSource.getPhotoDescription(photoData),
+            localSource.getCategories(photoData)
+        )
     }
 
-    override suspend fun getPhoto(photoName: String, category: String): DescribedPhoto =
-        cache[category]!!.first { it.name == photoName }.let {
-            it.toDescribedPhoto(
-                try {
-                    localSource.getPhotoDescription(it)
-                } catch (_: Exception) {
-                    "data couldn't be loaded for the requested photo"
-                }
-            )
+
+    private suspend fun makeCaches(list: List<PhotoData>) {
+        list.forEach { photoData ->
+            val categories = try {
+                localSource.getCategories(photoData)
+            } catch(_: Exception) {
+                emptyList()
+            }
+
+            categories.forEach { category ->
+                cache[category]?.add(photoData) ?: cache.put(category, mutableListOf(photoData))
+            }
         }
+    }
 
 }
--- app/src/main/java/com/kongedxz/appfiore/domain/repository/GalleryRepository.kt
+++ app/src/main/java/com/kongedxz/appfiore/domain/repository/GalleryRepository.kt
@@ -1,9 +1,9 @@
 package com.kongedxz.appfiore.domain.repository
 
 import com.kongedxz.appfiore.data.local.photos.PhotoData
-import com.kongedxz.appfiore.domain.entity.DescribedPhoto
+import com.kongedxz.appfiore.domain.entity.Photo
 
 interface GalleryRepository {
     suspend fun getPhotos(category: String): List<PhotoData>
-    suspend fun getPhoto(photoName: String, category: String): DescribedPhoto
+    suspend fun getPhoto(name: String): Photo?
 }
